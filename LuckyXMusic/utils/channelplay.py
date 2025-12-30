7be8edc4e047be0fe70bae7bbb0a9ab5efb44e29
Lucky Raja <166695239+The-LuckyX@users.noreply.github.com>
2025-12-31 01:12:34 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ LuckyXMusic/utils/channelplay.py
@@ -0,0 +1,23 @@
+from LuckyXMusic import Lucky
+from LuckyXMusic.utils.database import get_cmode
+
+
+async def get_channeplayCB(_, command, CallbackQuery):
+    if command == "c":
+        chat_id = await get_cmode(CallbackQuery.message.chat.id)
+        if chat_id is None:
+            try:
+                return await CallbackQuery.answer(_["setting_7"], show_alert=True)
+            except:
+                return
+        try:
+            channel = (await Lucky.get_chat(chat_id)).title
+        except:
+            try:
+                return await CallbackQuery.answer(_["cplay_4"], show_alert=True)
+            except:
+                return
+    else:
+        chat_id = CallbackQuery.message.chat.id
+        channel = None
+    return chat_id, channel
